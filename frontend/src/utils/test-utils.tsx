d9004c50ec4b3b604850b476f1778a31cde9dda1
KelviPascoal <kelvi_ugo@hotmail.com>
2025-07-25 19:43:34 -0300

    feat: configura a base do projeto

---
+++ frontend/src/utils/test-utils.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from 'react';
+import { render as rtlRender, RenderOptions } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../styles/theme';
+import '@testing-library/jest-dom';
+
+type AllTheProvidersProps = {
+  children: ReactNode;
+};
+
+function AllTheProviders({ children }: AllTheProvidersProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      {children}
+    </ThemeProvider>
+  );
+}
+
+const customRender = (
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) => rtlRender(ui, { wrapper: AllTheProviders, ...options });
+
+export * from '@testing-library/react';
+
+export { customRender as render };
