877b85451e05aa557aeda525d8c828c7ade7a3c3
KelviPascoal <kelvi_ugo@hotmail.com>
2025-08-01 17:10:23 -0300

    refactor: adiciona prop disabled para input

--- frontend/src/components/Form/Input/index.tsx
+++ frontend/src/components/Form/Input/index.tsx
@@ -2,10 +2,12 @@ import { InputHTMLAttributes } from 'react'
 import { FormField, FormFieldProps } from '../FormField'
 import * as S from './styles'
 
-export type InputProps = InputHTMLAttributes<HTMLInputElement> & Omit<FormFieldProps, 'children'>;
+export type InputProps = InputHTMLAttributes<HTMLInputElement> & Omit<FormFieldProps, 'children'> & {
+  isDisabled?: boolean
+};
 
-export const Input = ({ errorMessage, icon, label, hasError, ...props }: InputProps) => (
+export const Input = ({ errorMessage, icon, label, hasError, isDisabled, ...props }: InputProps) => (
   <FormField label={label} hasError={hasError} errorMessage={errorMessage} icon={icon}>
-    <S.InputField {...props} />
+    <S.InputField disabled={isDisabled} {...props} />
   </FormField>
 )
